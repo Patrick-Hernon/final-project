65b42a9e629954badb7e37d6f5443020a139e900
patrick-hernon <patrick.ginza@gmail.com>
2021-05-26 19:18:11 -0400

    Finishing Touches

---
+++ app.js
@@ -0,0 +1,64 @@
+//const greyButton = document.getElementById('grayButton')
+
+// greyButton.addEventListener('click', () => {
+//     document.body.style.backgroundColor = 'gray'
+//     document.body.style.color = 'white'
+//     document.body.style.color = 'teal'
+// })
+
+// const whiteButton = document.getElementById('whiteButton')
+
+// whiteButton.addEventListener('click', () => {
+//     document.body.style.color = 'green'
+//     document.body.style.backgroundImage = "url('https://pbs.twimg.com/profile_images/1080855598298611713/lTS-u1Iu_400x400.jpg')"
+// })
+
+const wheatButton = document.getElementById('wheatButton')
+
+wheatButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'orange'
+    document.body.style.backgroundImage = "url(https://i.redd.it/b1n1hsetd1p41.jpg)"
+    div.innerText = "Chef A world-renowned culinary artist, also known as the preparer of foodstuffs"
+})
+
+const redButton = document.getElementById('redButton')
+
+redButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'purple'
+    document.body.style.backgroundImage = "url(https://1.bp.blogspot.com/-cRRrUwjyuH8/VzUj7TXCBSI/AAAAAAAAKkI/5gYs4BNMtmgV7759MRMpNYwYzlwthJ3fQCLcB/s1600/Beaker.jpg)"
+    div.innerText = "Beaker Assistant inventor, Muppet Labs co-founder & Assistant Scientist"
+})
+
+const blackButton = document.getElementById('blackButton')
+
+blackButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'pink'
+    document.body.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBSlpz3_XJ4jQlAg10eyHuUOLliFXH6yIALg&usqp=CAU)"
+    div.innerText = "Gonzo Known as the most psychedelic daredevil & jack of all trades. He might be the most loved Mjuppet of all"
+})
+
+const greenButton = document.getElementById('greenButton')
+
+greenButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'black'
+    document.body.style.backgroundImage = "url('https://pbs.twimg.com/media/CwLwPKEWYAAPzyh.jpg')"
+    div.innerText = "Statler & Waldorf Long time friends & in-house front row hecklers of late night TV. Also know as The Grumpy Old Men."
+})
+
+const yellowButton = document.getElementById('yellowButton')
+
+yellowButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'blue'
+    document.body.style.backgroundImage = "url('https://pbs.twimg.com/profile_images/1080855598298611713/lTS-u1Iu_400x400.jpg')"
+    div.innerText = "Kermit the Frog An international star of the highest magnitude and worlds most famous anphibian. Who doesn't know Kermit?!"
+})    
+
+const brownButton = document.getElementById('brownButton')
+
+brownButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'white'
+    document.body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/en/e/e7/Animal_%28Muppet%29.jpg)"
+    div.innerText = "Animal The wild monster drummer of Dr. Teeth and the Electric Mayhem. Often being compared to to druummers such as The Who's Keith Moom & Led Zeppelin's John Bonham"
+})  
+
+const div = document.getElementById('text')
